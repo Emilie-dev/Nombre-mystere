a2a7cb3f36538e1dedbbd76a94be6abe71e1a27d
Chauvin <chauvin-e@orange.fr>
2017-03-10 13:32:34 +0100

    Correction avec Aurélien de l'exercice du Nombre mystere.

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 var solution = Math.floor(Math.random() * 4);
 console.log(solution);
 
-var response = prompt("Choisir un nombre entre 1 et 3.");
-console.log(response);
 
-for (var i = 3, i = 0, i--) {
-if (response === solution) {
-console.log("Code accepted!");
-break;                                                                                           
+for (var i = 3   ;     i > 0    ;   i--) {
+
+
+	var response = prompt("Choisir un nombre entre 1 et 3.");
+	console.log(response);
+
+	if (response == solution) {
+	console.log("Code accepted!");
+	break;                                                                                           
+	}
+
+	else if (response < solution)  {
+	console.log("Trop petit!");
+	}
+
+	else {
+	console.log("Trop grand!");
+	}
 }
 
-else if (response < solution)  {
-console.log("Trop petit!");
+
+if (i==0) {
+console.log("Erreur!");
 }
 
 else {
-console.log("Trop grand!");
-}
-}
-
-if (i==) {
-console.log();
-}else {
-console.log("Erreur!");
+console.log("GAGNÉ!");
 }
