bc6831e8822fa054c14377cf904634aec9c5551b
Chauvin <chauvin-e@orange.fr>
2017-03-10 10:56:38 +0100

    if/else vérification réponse, trop grand ou trop petit.

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ console.log(solution);
 var response = prompt("What's your name?", "Elisabeth")
 console.log(prompt);
 
-//Vérifier si la réponse de l'utilisateur est bonne.Si c'est le cas mettre "Code accepted!".
-//Si la réponse est trop grande ou trop petite afficher "Erreur!".
-if (response = "Elisabeth") {
-return "Code accepted!                                                                                           
-
+if (reponse = solution) {
+return "Code accepted!"                                                                                           
+}elsif (reponse < solution)  {
+return "Trop petit!"
 }else {
-return "Erreur!"
+return "Trop grand!"
 }
