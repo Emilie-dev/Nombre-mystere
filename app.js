bf6db2e0c1da42082c5dbbb18f3ae0e0250c454f
Chauvin <chauvin-e@orange.fr>
2017-03-10 10:15:55 +0100

    MAJ emplacement du fichier JS

---
+++ app.js
@@ -0,0 +1,14 @@
+var solution = Math.floor(Math.random() * 4);
+console.log(solution);
+
+var response = prompt("What's your name?", "Elisabeth")
+console.log(prompt);
+
+//Vérifier si la réponse de l'utilisateur est bonne.Si c'est le cas mettre "Code accepted!".
+//Si la réponse est trop grande ou trop petite afficher "Erreur!".
+if (response = "Elisabeth") {
+return "Code accepted!                                                                                           
+
+}else {
+return "Erreur!"
+}
