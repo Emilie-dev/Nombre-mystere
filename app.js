c6d9eaca4f1471893139e699d3d6fe00ff5fe8d8
Chauvin <chauvin-e@orange.fr>
2017-03-10 12:02:28 +0100

    Correction exercice Nombre mystere.

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ console.log(solution);
 var response = prompt("Choisir un nombre entre 1 et 3.");
 console.log(response);
 
+for (var i = 3, i = 0, i--) {
 if (response === solution) {
-console.log("Code accepted!");                                                                                           
-}else if (response < solution)  {
-console.log("Trop petit!");
-}else {
-console.log("Trop grand!");
+console.log("Code accepted!");
+break;                                                                                           
 }
 
+else if (response < solution)  {
+console.log("Trop petit!");
+}
+
+else {
+console.log("Trop grand!");
+}
+}
+
+if (i==) {
+console.log();
+}else {
+console.log("Erreur!");
+}
